6fb59b2c922a1ab9e9c60a994c117e278e66379d
Paras Jain <parasjain@parasjain.com>
2016-03-06 13:09:31 -0500

    Update submission

--- Project2/submit.sh
+++ Project2/submit.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-rm submit.zip
-zip -r submit.zip BinaryCSP.py autograder.out Extra
+rm submit.zip autograder.out extracredit.out
+python autograder.py > autograder.out
+python autograder.py -t ExtraCredit/6queens.test > extracredit.out  
+zip -r submit.zip BinaryCSP.py ExtraCredit autograder.out extracredit.out
